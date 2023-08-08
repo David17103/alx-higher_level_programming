67b59c84562e231f25fb7f6a74ab8e51a2a0e3b8
David17103 <davida.chetty12@gmail.com>
2023-08-08 00:13:31 -0700

    0x01 python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for numbers in range(10):
+    for single_digit in range(numbers + 1, 10):
+        print("{:02d}".format(numbers * 10 + single_digit), end="")
+        if single_digit != 9 or single_digit != 8:
+            print(", ", end="")
+        else:
+            print()
